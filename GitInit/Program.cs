918156b4728a74493e949694184522a8d03ee560
Snigger <fabirshe@gmail.com>
2021-02-07 16:58:54 +0300

    Sniggers Forever!

---
+++ GitInit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitInit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
